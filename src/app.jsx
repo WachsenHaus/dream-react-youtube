57736ec4b4a5235ee7be126dc70cf73b53c76b38
Younghoon CHOI _^ <wachsenhaus@gmail.com>
2020-12-17 23:56:03 +0900

    <추가>: <영상 클릭시 detail나오는 기능 추가함>

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,16 @@ import styles from "./app.module.css";
 import React, { useEffect, useState } from "react";
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 
 const App = ({ youtube }) => {
-  const [videos, setVideos] = useState(0);
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    console.log(video);
+    setSelectedVideo(video);
+  };
   const search = (query) => {
     youtube
       .search(query) //
@@ -20,7 +27,20 @@ const App = ({ youtube }) => {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      {Array.isArray(videos) && <VideoList videos={videos} />}
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 };
